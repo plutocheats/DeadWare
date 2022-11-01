536d5cd8990d4a018a63ba2797c9116415626577
pluto-guy <116435424+pluto-guy@users.noreply.github.com>
2022-11-01 22:07:35 +0000

    Update LoadID.lua

--- LoadID.lua
+++ LoadID.lua
@@ -5,3 +5,7 @@ end
 elseif game.PlaceId == 8619263259 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/plutocheats/DeadWare/main/Critical%20Legends.lua"))();
 end
+
+elseif game.PlaceId == 7860844204 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/plutocheats/DeadWare/main/Life%20sentence.lua"))();
+end
