15a57f97a0814b6bb03a7174a8f9c182b7836600
pluto-guy <116435424+pluto-guy@users.noreply.github.com>
2022-11-02 07:51:53 +0000

    Update LoadID.lua

--- LoadID.lua
+++ LoadID.lua
@@ -1,11 +1,7 @@
 if game.PlaceId == 3101667897 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/plutocheats/DeadWare/main/Legends-Of-Speed.lua"))();
-end
-
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/plutocheats/DeadWare/main/Legends-Of-Speed.lua"))();
 elseif game.PlaceId == 8619263259 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/plutocheats/DeadWare/main/Critical%20Legends.lua"))();
-end
-
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/plutocheats/DeadWare/main/Critical%20Legends.lua"))();
 elseif game.PlaceId == 7860844204 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/plutocheats/DeadWare/main/Life%20sentence.lua"))();
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/plutocheats/DeadWare/main/Life%20sentence.lua"))();
 end
