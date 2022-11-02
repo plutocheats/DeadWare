eaedb31922e54aea211c58b5d5b045563af98896
pluto-guy <116435424+pluto-guy@users.noreply.github.com>
2022-11-02 16:36:00 +0000

    Update LoadID.lua

--- LoadID.lua
+++ LoadID.lua
@@ -4,4 +4,6 @@ elseif game.PlaceId == 8619263259 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/plutocheats/DeadWare/main/Critical%20Legends.lua"))();
 elseif game.PlaceId == 7860844204 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/plutocheats/DeadWare/main/Life%20sentence.lua"))();
+elseif game.PlaceId == 8884334497 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/plutocheats/DeadWare/main/Mining%20Clicker.lua"))();
 end
