5195e1ee94a48e50bb57686479f43734fb1c7daa
Ad <95490130+AlexDevlpr@users.noreply.github.com>
2022-11-05 22:05:47 +0530

    Update LoadID.lua

--- LoadID.lua
+++ LoadID.lua
@@ -7,3 +7,20 @@ elseif game.PlaceId == 7860844204 then
 elseif game.PlaceId == 8884334497 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/plutocheats/DeadWare/main/Mining%20Clicker.lua"))();
 end
+
+local supported_games = {
+    [3101667897] = "https://raw.githubusercontent.com/plutocheats/DeadWare/main/Legends-Of-Speed.lua",
+    [8619263259] = "https://raw.githubusercontent.com/plutocheats/DeadWare/main/Critical%20Legends.lua",
+    [7860844204] = "https://raw.githubusercontent.com/plutocheats/DeadWare/main/Life%20sentence.lua",
+    [8884334497] = "https://raw.githubusercontent.com/plutocheats/DeadWare/main/Mining%20Clicker.lua"
+}
+
+local success, message = pcall(function()
+  loadstring(game:HttpGet(supported_games[game.PlaceId]))()
+end)
+
+if success then
+print("Thanks for choosing deadware, Script loaded.")
+else
+print("Game not supported!")
+end
